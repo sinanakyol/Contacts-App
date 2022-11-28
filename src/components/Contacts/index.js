3eb9b40b677da67f4069c22293952f19f270037c
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-11-28 04:43:11 +0300

    files added

---
+++ src/components/Contacts/index.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from "react";
+
+import List from "./List";
+import Form from "./Form";
+function Contacts() {
+  const [contacts, setContacts] = useState([
+    {
+      fullname: "Sinan",
+      phone_number: "123123",
+    },
+    {
+      fullname: "Ali",
+      phone_number: "456456",
+    },
+    {
+      fullname: "Nazlı",
+      phone_number: "987876",
+    },
+  ]);
+
+  useEffect(() => {
+    console.log(contacts);
+  }, [contacts]);
+
+  return (
+    <div id="container">
+      <h1>Contacts</h1>
+      <List contacts={contacts} />
+      <Form addContact={setContacts} contacts={contacts} />
+    </div>
+  );
+}
+
+export default Contacts;
