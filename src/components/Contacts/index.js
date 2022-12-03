9c85083681e41146ccf25f472d681451b36badd9
sinan akyol <110792519+sinanakyol@users.noreply.github.com>
2022-12-03 23:29:20 +0300

    index.js changed

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -24,7 +24,7 @@ function Contacts() {
 
   return (
     <div id="container">
-      <h1>Contacts</h1>
+      <h1>My Contacts</h1>
       <List contacts={contacts} />
       <Form addContact={setContacts} contacts={contacts} />
     </div>
